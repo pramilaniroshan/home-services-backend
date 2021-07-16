519091eff8580ff4dcd595a236b265bbe106d718
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-16 12:19:47 +0530

    user controller created

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,20 @@
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+exports.allAccess = (req, res) => {
+    res.status(200).send("Public Content.");
+  };
+  
+  exports.userBoard = (req, res) => {
+    res.status(200).send("User Content.");
+  };
+  
+  exports.adminBoard = (req, res) => {
+    res.status(200).send("Admin Content.");
+  };
+  
+  exports.moderatorBoard = (req, res) => {
+    res.status(200).send("Moderator Content.");
+  };
