4ff28de4b96b7f7abcc4d2cb0980b3bc06746383
pramilaniroshan <pramilaniroshan@gmail.com>
2021-08-01 12:21:22 +0530

    create service controller updated

--- app/controllers/service.controller.js
+++ app/controllers/service.controller.js
@@ -12,8 +12,16 @@ exports.allServices = (req, res) => {
   }
   
   exports.ServicesById = (req, res) => {
+    console.log(req.params.id)
     Service.findById(req.params.id)
-      .then(Service => res.json(Service))
+      .then(Service => res.json(Service) )
+      .catch(err => res.status(404).json({ noServicefound: 'No Service found' }));
+  }
+
+  exports.ServicesByUser = (req, res) => {
+    console.log(req.params.id)
+    Service.find( {user_id : req.params.id})
+      .then(Service => res.json(Service) )
       .catch(err => res.status(404).json({ noServicefound: 'No Service found' }));
   }
 
@@ -33,7 +41,19 @@ exports.allServices = (req, res) => {
   
   exports.UpdateId =(req, res) => {
     Service.findByIdAndUpdate(req.params.id, req.body)
-      .then(Service => res.json({ msg: 'Updated successfully' }))
+      .then(Service => 
+         res.status(200).send({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        rols: authorities,
+        accessToken: token,
+        zipcode : user.zipcode,
+        facebook : user.facebook,
+        mobile : user.mobile,
+        skype : user.skype
+        
+      }))
       .catch(err =>
         res.status(400).json({ error: 'Unable to update the Database' })
       );
